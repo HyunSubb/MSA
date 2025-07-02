57bbb7b916c48aee06bbc3a9798a9e492170ed92
신현섭 <didakd22@naver.com>
2025-07-02 22:52:57 +0900

    section02_07 완료

--- ordering/src/main/java/com/example/ordersystem/ordering/service/OrderingService.java
+++ ordering/src/main/java/com/example/ordersystem/ordering/service/OrderingService.java
@@ -1,14 +1,9 @@
 package com.example.ordersystem.ordering.service;
 
-import com.example.ordersystem.member.domain.Member;
-import com.example.ordersystem.member.repository.MemberRepository;
 import com.example.ordersystem.ordering.domain.Ordering;
 import com.example.ordersystem.ordering.dto.OrderCreateDto;
 import com.example.ordersystem.ordering.repository.OrderingRepository;
-import com.example.ordersystem.product.domain.Product;
-import com.example.ordersystem.product.repository.ProductRepository;
 import jakarta.persistence.EntityNotFoundException;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,29 +12,28 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class OrderingService {
     private final OrderingRepository orderingRepository;
-    private final MemberRepository memberRepository;
-    private final ProductRepository productRepository;
 
-    public OrderingService(OrderingRepository orderingRepository, MemberRepository memberRepository, ProductRepository productRepository) {
+    public OrderingService(OrderingRepository orderingRepository) {
         this.orderingRepository = orderingRepository;
-        this.memberRepository = memberRepository;
-        this.productRepository = productRepository;
     }
 
-    public Ordering orderCreate(OrderCreateDto orderDto){
-        String id = SecurityContextHolder.getContext().getAuthentication().getName();
-        Member member = memberRepository.findById(Long.parseLong(id)).orElseThrow(()-> new EntityNotFoundException("member is not found"));
+    public Ordering orderCreate(OrderCreateDto orderDto , String userId){
+//        이제 이 부분이 직접 db에서 proudct를 조회하는 것이 아닌 product 서버한테 http 요청을 보내야 한다.
+        Product product = productRepository.findById(orderDto.getProductId()).orElseThrow(()->
+                new EntityNotFoundException("product is not found"));
 
-        Product product = productRepository.findById(orderDto.getProductId()).orElseThrow(()->new EntityNotFoundException("product is not found"));
         int quantity = orderDto.getProductCount();
         if(product.getStockQuantity() < quantity){
             throw new IllegalArgumentException("재고 부족");
         }else {
+//            재고 감소 요청 또한 http 요청을 이용할 것임.
+//            여기서 http 요청에 두 가지 방법이 있다. 동기와 비동기
             product.updateStockQuantity(orderDto.getProductCount());
         }
+
         Ordering ordering = Ordering.builder()
-                .member(member)
-                .product(product)
+                .memberId(Long.parseLong(userId))
+                .productId(orderDto.getProductId())
                 .quantity(orderDto.getProductCount())
                 .build();
         orderingRepository.save(ordering);
